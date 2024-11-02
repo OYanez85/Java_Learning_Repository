4a1a0bafccd540318573940424ac496bd50e100a
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-11-02 10:12:45 +0100

    Update 0.6.4_Classes_Types_&_For_Each_Loops.java

--- Chapter_1_Dive_in/0.6.4_Classes_Types_&_For_Each_Loops.java
+++ Chapter_1_Dive_in/0.6.4_Classes_Types_&_For_Each_Loops.java
@@ -25,11 +25,11 @@ System.out.println(f.geta());
 System.out.println(g.geta());
 
 // What is printed when this code is executed?
-A) 14
-    8
-B)  6 
-   14
-C)  6
-    8
-D) 14
-   14
+   A) 14
+       8
+// B)  6 
+//    14
+// C)  6
+//     8
+// D) 14
+//    14

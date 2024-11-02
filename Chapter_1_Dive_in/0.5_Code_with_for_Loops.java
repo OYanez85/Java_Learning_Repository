541fbdc180ed9721faa6e34c99f085d4fa77df80
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-11-02 08:46:03 +0100

    Update 0.5_Code_with_for_Loops.java

--- Chapter_1_Dive_in/0.5_Code_with_for_Loops.java
+++ Chapter_1_Dive_in/0.5_Code_with_for_Loops.java
@@ -1,6 +1,7 @@
 import edu.duke.FileResource;  // Import the FileResource class from the edu.duke package
 
 public class HelloWorld {  // Define a public class named HelloWorld
+    
     public void runHello() {  // Define a public method named runHello
         FileResource f;  // Declare a FileResource object named f
         f = new FileResource("file.txt");  // Initialize the FileResource object with "file.txt"
@@ -10,4 +11,10 @@ public class HelloWorld {  // Define a public class named HelloWorld
             System.out.println(line);  // Print each line to the console
         }
     }
+    
+    public static void main(String[] args) {  // Define the main method as the entry point of the program
+        HelloWorld hw = new HelloWorld();  // Create an instance of HelloWorld called hw
+        hw.runHello();  // Call the runHello method on the HelloWorld instance
+    }
 }
+

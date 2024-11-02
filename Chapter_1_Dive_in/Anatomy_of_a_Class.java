d5ac5d6a514c4068b2ca3437c0d1e9207549f758
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-11-02 08:11:03 +0100

    Create Anatomy_of_a_Class.java

---
+++ Chapter_1_Dive_in/Anatomy_of_a_Class.java
@@ -0,0 +1,15 @@
+public class MyFirstApp { // Public so everyone can access it; this is a class; the name of the class
+
+    public static void main (String[] args) { // The main method of this class
+        // 'public' so everyone can access it
+        // 'static' - we'll cover this later
+        // 'void' means there's no return value
+        // 'main' - the name of this method
+        // 'String[] args' - arguments to the method; it's an array of Strings called 'args'
+        
+        System.out.print("I Rule!"); // This says to print to standard output (defaults to command line)
+        // "I Rule!" - the String you want to print
+        // Every statement MUST end in a semicolon!
+    } // Closing brace of the main method
+    
+} // Closing brace of the MyFirstApp class

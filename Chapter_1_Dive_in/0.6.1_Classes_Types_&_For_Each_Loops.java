fa0a18cac535d734f2eb581bde438e6cffd4582c
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-11-02 09:37:47 +0100

    Update 0.6.1_Classes_Types_&_For_Each_Loops.java

--- Chapter_1_Dive_in/0.6.1_Classes_Types_&_For_Each_Loops.java
+++ Chapter_1_Dive_in/0.6.1_Classes_Types_&_For_Each_Loops.java
@@ -1,16 +1,18 @@
-public class Thing {
+public class Thing {                // Define a public class named Thing
   
-  private int a;
+  private int a;                    // Declare a private integer variable a
   
-  public Thing(int x) {
-    a = x;
+  public Thing(int x) {             // Define a constructor for the Thing class that takes an integer x
+    a = x;                          // Assign the value of x to the instance variable a
   }
   
-  public int geta() {
-    return a;
+  public int geta() {               // Define a public method named geta that returns an integer
+    return a;                       // Return the value of the instance variable a
   }
   
-  public void print() {
-    int b = 4;System.out.println(geta() + " " + b);
+  public void print() {             // Define a public method named print with no return type
+    int b = 4;                      // Declare a local integer variable b and assign it the value 4
+    System.out.println(geta() + " " + b);  // Print the value of a (from geta()) and b, separated by a space
   }
 }
+

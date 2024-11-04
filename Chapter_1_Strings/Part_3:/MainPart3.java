1b1a5307fcb360babbf84d149f30e097994dea39
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-11-04 09:43:41 +0100

    Create MainPart3.java

---
+++ Chapter_1_Strings/Part_3:/MainPart3.java
@@ -0,0 +1,7 @@
+public class MainPart3 {
+    public static void main(String[] args) {
+        Part3 part3 = new Part3();  // Create an instance of Part3
+        System.out.println("Running Part3 tests...");
+        part3.testing();  // Run the test method for Part3
+    }
+}

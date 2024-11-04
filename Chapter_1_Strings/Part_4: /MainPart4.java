f2cae636131576592b9f5e6f4f86198a7df35589
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-11-04 09:44:11 +0100

    Create MainPart4.java

---
+++ Chapter_1_Strings/Part_4: /MainPart4.java
@@ -0,0 +1,7 @@
+public class MainPart4 {
+    public static void main(String[] args) {
+        Part4 part4 = new Part4();  // Create an instance of Part4
+        System.out.println("Running Part4 tests...");
+        part4.findYouTubeLinks();  // Run the method to find YouTube links in Part4
+    }
+}

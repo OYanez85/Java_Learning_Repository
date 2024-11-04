6451ac3a16971106e3e992c6fd731b02bbb27a22
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-11-04 11:03:21 +0100

    Delete Chapter_2_Strings/Part_1:_Finding_a_Gene directory

--- Chapter_2_Strings/Part_1:_Finding_a_Gene/MainPart1.java
+++
@@ -1,7 +0,0 @@
-public class MainPart1 {
-    public static void main(String[] args) {
-        Part1 part1 = new Part1();  // Create an instance of Part1
-        System.out.println("Running Part1 tests...");
-        part1.testSimpleGene();  // Run the test method for Part1
-    }
-}

2e9755b53c6e6e21204ff9d640e2de9bda8569ee
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-11-04 09:42:11 +0100

    Create MainPart2.java

---
+++ Chapter_1_Strings/Part_2_Finding_a_Gene/MainPart2.java
@@ -0,0 +1,7 @@
+public class MainPart2 {
+    public static void main(String[] args) {
+        Part2 part2 = new Part2();  // Create an instance of Part2
+        System.out.println("Running Part2 tests...");
+        part2.testSimpleGene();  // Run the test method for Part2
+    }
+}
